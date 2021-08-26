cc67a4233b7e276c0e5e59d65f0ba5bbd5bb9de5
ifernandez <fernandezpij@gmail.com>
2021-08-26 12:32:27 -0500

    commit

--- src/main/java/com/incloud/hcp/rest/GeneralRest.java
+++ src/main/java/com/incloud/hcp/rest/GeneralRest.java
@@ -49,6 +49,18 @@ public class GeneralRest {
 
     }
 
+    @PostMapping(value = "/AppMaestros/", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<AppMaestrosExports> AppMaestros(@RequestBody AppMaestrosImports imports){
+
+        try {
+            return Optional.ofNullable(this.MaestroService.appMaestros(imports))
+                    .map(l -> new ResponseEntity<>(l, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (Exception e) {
+            //String error = Utils.obtieneMensajeErrorException(e);
+            throw new RuntimeException(e.toString());
+        }
+
+    }
 
 
 }
