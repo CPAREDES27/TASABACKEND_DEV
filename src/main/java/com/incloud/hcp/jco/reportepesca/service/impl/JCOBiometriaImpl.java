469756ed72d465ef5300cb8db3c2f12c159720a7
ifernandez <fernandezpij@gmail.com>
2021-09-06 22:43:46 -0500

    Reporte Biometria

--- src/main/java/com/incloud/hcp/jco/reportepesca/service/impl/JCOBiometriaImpl.java
+++ src/main/java/com/incloud/hcp/jco/reportepesca/service/impl/JCOBiometriaImpl.java
@@ -10,11 +10,10 @@ import com.incloud.hcp.util.Tablas;
 import com.sap.conn.jco.*;
 import javafx.scene.control.Tab;
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -24,6 +23,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import org.apache.commons.codec.binary.Base64;
 
@@ -39,15 +39,9 @@ public class JCOBiometriaImpl implements JCOBiometriaService {
             JCoDestination destination = JCoDestinationManager.getDestination(Constantes.DESTINATION_NAME);
             JCoRepository repo = destination.getRepository();
             JCoFunction function = repo.getFunction(Constantes.ZFL_RFC_GPES_CONS_BIOM);
-            JCoParameterList paramImport = function.getImportParameterList();
-            logger.error("lista import: "+function.getImportParameterList());
-            logger.error("lista tablas: "+function.getTableParameterList());
-            logger.error("lista export: "+function.getExportParameterList());
-            logger.error("lista export: "+paramImport.getTable("IT_MAREA"));
 
             HashMap<String, Object> importParams = new HashMap<>();
             importParams.put("IP_OPER", imports.getIp_oper());
-            // importParams.put("IT_MAREA", imports.getIt_marea());
             importParams.put("IP_CDMMA", imports.getIp_cdmma());
 
             JCoParameterList importx = function.getImportParameterList();
@@ -69,10 +63,9 @@ public class JCOBiometriaImpl implements JCOBiometriaService {
             List<HashMap<String, Object>> et_espe = metodos.ListarObjetos(ET_ESPE);
             List<HashMap<String, Object>> et_pscinc = metodos.ListarObjetos(ET_PSCINC);
 
-
             String path=CrearExcel(ET_BIOM);
-            String base64=ConvertirABase64(path);
 
+            String base64=ConvertirABase64(path);
             dto.setEt_biom(et_biom);
             dto.setEt_espe(et_espe);
             dto.setEt_pscinc(et_pscinc);
@@ -88,36 +81,50 @@ public class JCOBiometriaImpl implements JCOBiometriaService {
     private String CrearExcel(JCoTable jCoTable){
         String path="";
         Workbook workbook = new HSSFWorkbook();
+
         //Crea hoja nueva
         Sheet sheet = workbook.createSheet("Reporte Biometria");
         //Por cada línea se crea un arreglo de objetos (Object[])
         Map<String, Object[]> datos = new TreeMap<String, Object[]>();
         Object[] fields= ListarFields(jCoTable);
+        List<String[]> registros=ListarRegistros(jCoTable);
+
         //datos.put("1", new Object[]{"Identificador", "Nombre", "Apellidos"});
+        String[]r=registros.get(0);
+        for (int j=0; j<r.length; j++){
+            logger.error("arrayString: "+ r[0]);
+        }
+        //datos.put("1", new Object[]{});
         datos.put("1", fields);
 
+        int keys=2;
+        for(int i=0; i<registros.size(); i++){
+            Object[] obj= registros.get(i);
+
+            datos.put(String.valueOf(keys), obj);
+            keys++;
+        }
 
 
-        datos.put("2", new Object[]{1, "María", "Remen"});
-        datos.put("3", new Object[]{2, "David", "Allos"});
-        datos.put("4", new Object[]{3, "Carlos", "Caritas"});
-        datos.put("5", new Object[]{4, "Luisa", "Vitz"});
         //Iterar sobre datos para escribir en la hoja
         Set keyset = datos.keySet();
         int numeroRenglon = 0;
         for (Object key : keyset) {
             Row row = sheet.createRow(numeroRenglon++);
+
             Object[] arregloObjetos = datos.get(key);
             int numeroCelda = 0;
             for (Object obj : arregloObjetos) {
                 Cell cell = row.createCell(numeroCelda++);
                 if (obj instanceof String) {
                     cell.setCellValue((String) obj);
+
                 } else if (obj instanceof Integer) {
                     cell.setCellValue((Integer) obj);
                 }
             }
         }
+
         try {
             //Se genera el documento
             path=Constantes.RUTA_ARCHIVO_IMPORTAR + "/Reporte.xlsx";
@@ -137,29 +144,29 @@ public class JCOBiometriaImpl implements JCOBiometriaService {
         return new String(encoded, StandardCharsets.UTF_8);
     }
 
-    private Object[] ListarFields(JCoTable jcoTable){
+    private String[] ListarFields(JCoTable jcoTable){
 
         JCoFieldIterator iter = jcoTable.getFieldIterator();
         int con=0;
+        String[] f=new String[100];
         while (iter.hasNextField()) {
             JCoField field = iter.nextField();
+            f[con]=field.getName();
             con++;
         }
-        Object[] fields= new Object[con];
-        int i=0;
-        while (iter.hasNextField()) {
-            JCoField field = iter.nextField();
-            fields[i]=field.getName();
-            con++;
+        String[] fields= new String[con];
+        for(int j=0; j<con; j++){
+            fields[j]=jcoTable.getField(j).getName();
+
         }
 
         return fields;
 
     }
 
-    private List<Object[]> ListarRegistros(JCoTable jcoTable){
+    private List<String[]> ListarRegistros(JCoTable jcoTable){
 
-        List<Object[]> campos= new ArrayList<Object[]>();
+        List<String[]> campos= new ArrayList<String[]>();
 
         JCoFieldIterator iter = jcoTable.getFieldIterator();
 
@@ -170,12 +177,23 @@ public class JCOBiometriaImpl implements JCOBiometriaService {
         }
         for (int i = 0; i < jcoTable.getNumRows(); i++) {
             jcoTable.setRow(i);
-            Object[] registros = new Object[con];
-            int j = 0;
-            while (iter.hasNextField()) {
-                JCoField field = iter.nextField();
+            String[] registros = new String[con];
+            JCoFieldIterator ite = jcoTable.getFieldIterator();
+            int j=0;
+            while (ite.hasNextField()) {
+                JCoField field = ite.nextField();
                 String key = (String) field.getName();
-                Object value = jcoTable.getValue(key);
+                String value ="";
+                try {
+                    value = jcoTable.getValue(key).toString();
+                }catch (Exception e){
+                    value="";
+                }
+
+                if (key.equals("HIEVN") || key.equals("HFEVN")) {
+                    //SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+                    value = value.substring(11,19);
+                }
                 registros[j] = value;
                 j++;
             }
