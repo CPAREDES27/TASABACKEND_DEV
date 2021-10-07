468b6bf4713c6f6127d02bee4a25f29b43492a4f
Israel <fernandezpij@gmail.com>
2021-10-07 18:03:37 -0500

    Plantilla PDF Trabajo FF

---
+++ src/main/java/com/incloud/hcp/jco/tripulantes/dto/PDFTrabajoFFConstantes.java
@@ -0,0 +1,15 @@
+package com.incloud.hcp.jco.tripulantes.dto;
+
+public class PDFTrabajoFFConstantes {
+
+    public static String titulo="TRABAJO FUERA DE FAENA/VIÁTICOS";
+    public static String numeroTrabajo="Nro Trabajo: ";
+    public static String tipoTrabajo="Tipo de trabajo: ";
+    public static String semana="Semana: ";
+    public static String fechaInicio="Fecha Inicio: ";
+    public static String fechaFin="Fecha Fin: ";
+    public static String descripcionTrabajo="Descripción del trabajo: ";
+    public static String observacion="Observación: ";
+
+
+}
---
+++ src/main/java/com/incloud/hcp/jco/tripulantes/dto/PDFTrabajoFFDto.java
@@ -0,0 +1,68 @@
+package com.incloud.hcp.jco.tripulantes.dto;
+
+public class PDFTrabajoFFDto {
+
+    private String numeroTrabajo;
+    private String tipoTrabajo;
+    private String semana;
+    private String fechaInicio;
+    private String fechaFin;
+    private String descripcionTrabajo;
+    private String observacion;
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public String getNumeroTrabajo() {
+        return numeroTrabajo;
+    }
+
+    public void setNumeroTrabajo(String numeroTrabajo) {
+        this.numeroTrabajo = numeroTrabajo;
+    }
+
+    public String getTipoTrabajo() {
+        return tipoTrabajo;
+    }
+
+    public void setTipoTrabajo(String tipoTrabajo) {
+        this.tipoTrabajo = tipoTrabajo;
+    }
+
+    public String getSemana() {
+        return semana;
+    }
+
+    public void setSemana(String semana) {
+        this.semana = semana;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getDescripcionTrabajo() {
+        return descripcionTrabajo;
+    }
+
+    public void setDescripcionTrabajo(String descripcionTrabajo) {
+        this.descripcionTrabajo = descripcionTrabajo;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+}
