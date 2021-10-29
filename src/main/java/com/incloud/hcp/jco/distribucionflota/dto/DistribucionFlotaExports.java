07cf2fe167c427328fb111eae7f004ceb0d5d450
Erick Acosta <erick-98@hotmail.com>
2021-10-29 10:22:53 -0500

    Subida de fuentes erick

--- src/main/java/com/incloud/hcp/jco/distribucionflota/dto/DistribucionFlotaExports.java
+++ src/main/java/com/incloud/hcp/jco/distribucionflota/dto/DistribucionFlotaExports.java
@@ -11,18 +11,114 @@ public class DistribucionFlotaExports {
     private List<TotalDto> ListaTotal;
 
     private String mensaje;
-    private String tot_desc_cbod;
-    private String tot_desc_dscl;
-    private String tot_desc_desc;
-    private String tot_terc_cbod;
-    private String tot_terc_dscl;
-    private String tot_terc_ep;
-    private String tot_prop_cbod;
-    private String tot_prop_dscl;
-    private String tot_prop_ep;
-    private String tot_tot_cbod;
-    private String tot_tot_dscl;
-    private String tot_tot_ep;
+    private double tot_desc_cbod;
+    private double tot_desc_dscl;
+    private double tot_desc_desc;
+    private double tot_terc_cbod;
+    private double tot_terc_dscl;
+    private double tot_terc_ep;
+    private double tot_prop_cbod;
+    private double tot_prop_dscl;
+    private double tot_prop_ep;
+    private double tot_tot_cbod;
+    private double tot_tot_dscl;
+    private double tot_tot_ep;
+
+    public double getTot_desc_cbod() {
+        return tot_desc_cbod;
+    }
+
+    public void setTot_desc_cbod(double tot_desc_cbod) {
+        this.tot_desc_cbod = tot_desc_cbod;
+    }
+
+    public double getTot_desc_dscl() {
+        return tot_desc_dscl;
+    }
+
+    public void setTot_desc_dscl(double tot_desc_dscl) {
+        this.tot_desc_dscl = tot_desc_dscl;
+    }
+
+    public double getTot_desc_desc() {
+        return tot_desc_desc;
+    }
+
+    public void setTot_desc_desc(double tot_desc_desc) {
+        this.tot_desc_desc = tot_desc_desc;
+    }
+
+    public double getTot_terc_cbod() {
+        return tot_terc_cbod;
+    }
+
+    public void setTot_terc_cbod(double tot_terc_cbod) {
+        this.tot_terc_cbod = tot_terc_cbod;
+    }
+
+    public double getTot_terc_dscl() {
+        return tot_terc_dscl;
+    }
+
+    public void setTot_terc_dscl(double tot_terc_dscl) {
+        this.tot_terc_dscl = tot_terc_dscl;
+    }
+
+    public double getTot_terc_ep() {
+        return tot_terc_ep;
+    }
+
+    public void setTot_terc_ep(double tot_terc_ep) {
+        this.tot_terc_ep = tot_terc_ep;
+    }
+
+    public double getTot_prop_cbod() {
+        return tot_prop_cbod;
+    }
+
+    public void setTot_prop_cbod(double tot_prop_cbod) {
+        this.tot_prop_cbod = tot_prop_cbod;
+    }
+
+    public double getTot_prop_dscl() {
+        return tot_prop_dscl;
+    }
+
+    public void setTot_prop_dscl(double tot_prop_dscl) {
+        this.tot_prop_dscl = tot_prop_dscl;
+    }
+
+    public double getTot_prop_ep() {
+        return tot_prop_ep;
+    }
+
+    public void setTot_prop_ep(double tot_prop_ep) {
+        this.tot_prop_ep = tot_prop_ep;
+    }
+
+    public double getTot_tot_cbod() {
+        return tot_tot_cbod;
+    }
+
+    public void setTot_tot_cbod(double tot_tot_cbod) {
+        this.tot_tot_cbod = tot_tot_cbod;
+    }
+
+    public double getTot_tot_dscl() {
+        return tot_tot_dscl;
+    }
+
+    public void setTot_tot_dscl(double tot_tot_dscl) {
+        this.tot_tot_dscl = tot_tot_dscl;
+    }
+
+    public double getTot_tot_ep() {
+        return tot_tot_ep;
+    }
+
+    public void setTot_tot_ep(double tot_tot_ep) {
+        this.tot_tot_ep = tot_tot_ep;
+    }
 
     public List<TotalDto> getListaTotal() {
         return ListaTotal;
@@ -54,102 +150,6 @@ public class DistribucionFlotaExports {
 
     public void setListaTerceros(List<TercerosDto> listaTerceros) {
         ListaTerceros = listaTerceros;
-    }
-
-    public String getTot_desc_cbod() {
-        return tot_desc_cbod;
-    }
-
-    public void setTot_desc_cbod(String tot_desc_cbod) {
-        this.tot_desc_cbod = tot_desc_cbod;
-    }
-
-    public String getTot_desc_dscl() {
-        return tot_desc_dscl;
-    }
-
-    public void setTot_desc_dscl(String tot_desc_dscl) {
-        this.tot_desc_dscl = tot_desc_dscl;
-    }
-
-    public String getTot_desc_desc() {
-        return tot_desc_desc;
-    }
-
-    public void setTot_desc_desc(String tot_desc_desc) {
-        this.tot_desc_desc = tot_desc_desc;
-    }
-
-    public String getTot_terc_cbod() {
-        return tot_terc_cbod;
-    }
-
-    public void setTot_terc_cbod(String tot_terc_cbod) {
-        this.tot_terc_cbod = tot_terc_cbod;
-    }
-
-    public String getTot_terc_dscl() {
-        return tot_terc_dscl;
-    }
-
-    public void setTot_terc_dscl(String tot_terc_dscl) {
-        this.tot_terc_dscl = tot_terc_dscl;
-    }
-
-    public String getTot_terc_ep() {
-        return tot_terc_ep;
-    }
-
-    public void setTot_terc_ep(String tot_terc_ep) {
-        this.tot_terc_ep = tot_terc_ep;
-    }
-
-    public String getTot_prop_cbod() {
-        return tot_prop_cbod;
-    }
-
-    public void setTot_prop_cbod(String tot_prop_cbod) {
-        this.tot_prop_cbod = tot_prop_cbod;
-    }
-
-    public String getTot_prop_dscl() {
-        return tot_prop_dscl;
-    }
-
-    public void setTot_prop_dscl(String tot_prop_dscl) {
-        this.tot_prop_dscl = tot_prop_dscl;
-    }
-
-    public String getTot_prop_ep() {
-        return tot_prop_ep;
-    }
-
-    public void setTot_prop_ep(String tot_prop_ep) {
-        this.tot_prop_ep = tot_prop_ep;
-    }
-
-    public String getTot_tot_cbod() {
-        return tot_tot_cbod;
-    }
-
-    public void setTot_tot_cbod(String tot_tot_cbod) {
-        this.tot_tot_cbod = tot_tot_cbod;
-    }
-
-    public String getTot_tot_dscl() {
-        return tot_tot_dscl;
-    }
-
-    public void setTot_tot_dscl(String tot_tot_dscl) {
-        this.tot_tot_dscl = tot_tot_dscl;
-    }
-
-    public String getTot_tot_ep() {
-        return tot_tot_ep;
-    }
-
-    public void setTot_tot_ep(String tot_tot_ep) {
-        this.tot_tot_ep = tot_tot_ep;
     }
 
     public String getMensaje() {
--- src/main/java/com/incloud/hcp/jco/distribucionflota/dto/TotalDto.java
+++ src/main/java/com/incloud/hcp/jco/distribucionflota/dto/TotalDto.java
@@ -3,10 +3,34 @@ package com.incloud.hcp.jco.distribucionflota.dto;
 public class TotalDto {
     private String CodPlanta;
     private String DescPlanta;
-    private String PescDeclProp;
-    private String EmbaPescProp;
-    private String CbodProp;
+    private double PescDeclProp;
+    private double EmbaPescProp;
+    private double CbodProp;
     private String dif;
+
+    public double getPescDeclProp() {
+        return PescDeclProp;
+    }
+
+    public void setPescDeclProp(double pescDeclProp) {
+        PescDeclProp = pescDeclProp;
+    }
+
+    public double getEmbaPescProp() {
+        return EmbaPescProp;
+    }
+
+    public void setEmbaPescProp(double embaPescProp) {
+        EmbaPescProp = embaPescProp;
+    }
+
+    public double getCbodProp() {
+        return CbodProp;
+    }
+
+    public void setCbodProp(double cbodProp) {
+        CbodProp = cbodProp;
+    }
 
     public String getCodPlanta() {
         return CodPlanta;
@@ -22,30 +46,6 @@ public class TotalDto {
 
     public void setDescPlanta(String descPlanta) {
         DescPlanta = descPlanta;
-    }
-
-    public String getPescDeclProp() {
-        return PescDeclProp;
-    }
-
-    public void setPescDeclProp(String pescDeclProp) {
-        PescDeclProp = pescDeclProp;
-    }
-
-    public String getEmbaPescProp() {
-        return EmbaPescProp;
-    }
-
-    public void setEmbaPescProp(String embaPescProp) {
-        EmbaPescProp = embaPescProp;
-    }
-
-    public String getCbodProp() {
-        return CbodProp;
-    }
-
-    public void setCbodProp(String cbodProp) {
-        CbodProp = cbodProp;
     }
 
     public String getDif() {
