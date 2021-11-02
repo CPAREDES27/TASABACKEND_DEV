a9a1635b17da600212ff7b650177118d77fc3a51
Future Technologies <cesargpq@gmail.com>
2021-11-02 15:03:32 -0500

    GGGGGGG

--- src/main/java/com/incloud/hcp/jco/controlLogistico/service/impl/JCOAceitesUsadosImpl.java
+++ src/main/java/com/incloud/hcp/jco/controlLogistico/service/impl/JCOAceitesUsadosImpl.java
@@ -82,7 +82,7 @@ public class JCOAceitesUsadosImpl implements JCOAceitesUsadosService {
             JCoTable T_RPN = tables.getTable(Tablas.T_RPN);
 
             //region
-            if(imports.getIp_tope().equals("NR")){
+
                 for(int i=0; i<imports.getT_rpn().size();i++) {
                     RegistroAceiteDto  dto = imports.getT_rpn().get(i);
                     logger.error("GGG"+imports.getT_rpn().get(i).getNRPOS());
@@ -128,7 +128,7 @@ public class JCOAceitesUsadosImpl implements JCOAceitesUsadosService {
                 T_RNV.setValue("HRMFN","000000");
                 T_RNV.setValue("ATMFN","");
 
-            }
+
             //endRegion
             stfcConnection.execute(destination);
 
