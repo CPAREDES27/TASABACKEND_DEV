e070363d5d3962cb74bfd8115509ec92f67cf654
Future Technologies <cesargpq@gmail.com>
2021-11-02 09:04:42 -0500

    ss

--- src/main/java/com/incloud/hcp/rest/AceitesUsadosRest.java
+++ src/main/java/com/incloud/hcp/rest/AceitesUsadosRest.java
@@ -42,6 +42,16 @@ public class AceitesUsadosRest {
             throw new RuntimeException(e.toString());
         }
     }
+    @PostMapping(value = "/Anular", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<AceitesUsadosExports> Anular(@RequestBody AceitesUsadosImports imports) {
 
+        try {
+            return Optional.ofNullable(this.jcoAceitesUsadosService.Anular(imports))
+                    .map(l -> new ResponseEntity<>(l, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (Exception e) {
+            //String error = Utils.obtieneMensajeErrorException(e);
+            throw new RuntimeException(e.toString());
+        }
+    }
 
 }
