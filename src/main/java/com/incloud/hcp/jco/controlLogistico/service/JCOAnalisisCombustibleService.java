ee83663f495732342281347c29d3c5b18367c867
Future Technologies <cesargpq@gmail.com>
2021-11-04 23:47:52 -0500

    fff

--- src/main/java/com/incloud/hcp/jco/controlLogistico/service/JCOAnalisisCombustibleService.java
+++ src/main/java/com/incloud/hcp/jco/controlLogistico/service/JCOAnalisisCombustibleService.java
@@ -1,12 +1,10 @@
 package com.incloud.hcp.jco.controlLogistico.service;
 
-import com.incloud.hcp.jco.controlLogistico.dto.AnalisisCombusLisExports;
-import com.incloud.hcp.jco.controlLogistico.dto.AnalisisCombusLisImports;
-import com.incloud.hcp.jco.controlLogistico.dto.AnalisisCombusImports;
-import com.incloud.hcp.jco.controlLogistico.dto.ControlLogExports;
+import com.incloud.hcp.jco.controlLogistico.dto.*;
 
 public interface JCOAnalisisCombustibleService {
 
     AnalisisCombusLisExports Listar(AnalisisCombusLisImports imports)throws Exception;
     ControlLogExports Detalle(AnalisisCombusImports imports)throws Exception;
+    QlikExport QlikView(QlikView imports)throws Exception;
 }
--- src/main/java/com/incloud/hcp/util/Tablas.java
+++ src/main/java/com/incloud/hcp/util/Tablas.java
@@ -11,6 +11,7 @@ public class Tablas {
     public static String STR_SET="STR_SET";
     public static String STR_EMB="STR_EMB";
     public static String ST_PDG="ST_PDG";
+    public static String STR_CEF="STR_CEF";
     public static String ZTFL_HISCOM="ZTFL_HISCOM";
     public static String S_DATA="S_DATA";
     public static String ST_CCP="ST_CCP";
