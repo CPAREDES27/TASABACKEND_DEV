ade7a627a389f14a932d558be1cc9c9385f09877
Future Technologies <cesargpq@gmail.com>
2021-11-08 15:16:31 -0500

    xd

--- src/main/java/com/incloud/hcp/jco/maestro/dto/BusquedaArmadorDTO.java
+++ src/main/java/com/incloud/hcp/jco/maestro/dto/BusquedaArmadorDTO.java
@@ -1,7 +1,27 @@
 package com.incloud.hcp.jco.maestro.dto;
 
+import java.util.List;
+
 public class BusquedaArmadorDTO {
     private String codigo;
+    private List<MaestroOptionsKey> options2;
+    private List<MaestroOptions> option;
+
+    public List<MaestroOptions> getOption() {
+        return option;
+    }
+
+    public void setOption(List<MaestroOptions> option) {
+        this.option = option;
+    }
+
+    public List<MaestroOptionsKey> getOptions2() {
+        return options2;
+    }
+
+    public void setOptions2(List<MaestroOptionsKey> options2) {
+        this.options2 = options2;
+    }
 
     public String getCodigo() {
         return codigo;
