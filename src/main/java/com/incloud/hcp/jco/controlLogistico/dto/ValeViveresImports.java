2cbc110eb4ea52b58e647499e66604fc751af16c
Future Technologies <cesargpq@gmail.com>
2021-11-17 09:01:06 -0500

    gg

--- src/main/java/com/incloud/hcp/jco/controlLogistico/dto/ValeViveresImports.java
+++ src/main/java/com/incloud/hcp/jco/controlLogistico/dto/ValeViveresImports.java
@@ -1,5 +1,8 @@
 package com.incloud.hcp.jco.controlLogistico.dto;
 
+import com.incloud.hcp.jco.maestro.dto.MaestroOptions;
+import com.incloud.hcp.jco.maestro.dto.MaestroOptionsKey;
+
 import java.util.List;
 
 public class ValeViveresImports {
@@ -7,15 +10,27 @@ public class ValeViveresImports {
     private String p_user;
     private String rowcount;
     private String[] fields;
-    private List<Options> options;
 
-    public List<Options> getOptions() {
-        return options;
+    private List<MaestroOptions> options1;
+    private List<MaestroOptionsKey> options2;
+
+    public List<MaestroOptions> getOptions1() {
+        return options1;
     }
 
-    public void setOptions(List<Options> options) {
-        this.options = options;
+    public void setOptions1(List<MaestroOptions> options1) {
+        this.options1 = options1;
     }
+
+    public List<MaestroOptionsKey> getOptions2() {
+        return options2;
+    }
+
+    public void setOptions2(List<MaestroOptionsKey> options2) {
+        this.options2 = options2;
+    }
+
+
 
     public String[] getFields() {
         return fields;
