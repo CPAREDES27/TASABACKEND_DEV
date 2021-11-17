f7c28f952f7948b4b1ceda89972c3d58d0633def
pierotoscano <ptoscano@xternal.biz>
2021-11-17 11:32:00 -0500

    Carga pesca histórico competencia

---
+++ src/main/java/com/incloud/hcp/rest/HistoricoCompetenciaRest.java
@@ -0,0 +1,31 @@
+package com.incloud.hcp.rest;
+
+import com.incloud.hcp.jco.maestro.dto.PescaCompCargaHistoricoExports;
+import com.incloud.hcp.jco.maestro.dto.PescaCompCargaHistoricoImports;
+import com.incloud.hcp.jco.maestro.service.JCOHistoricoCompetenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+@RequestMapping(value = "/api/historicocompetencia")
+public class HistoricoCompetenciaRest {
+    @Autowired
+    JCOHistoricoCompetenciaService jcoHistoricoCompetenciaService;
+
+    @PostMapping(value = "/cargahistorico/", produces = APPLICATION_JSON_VALUE)
+    private ResponseEntity<PescaCompCargaHistoricoExports> cargaHistorico(@RequestBody PescaCompCargaHistoricoImports imports) {
+        try {
+            return Optional.ofNullable(this.jcoHistoricoCompetenciaService.CargaHistorico(imports))
+                    .map(l -> new ResponseEntity<>(l, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (Exception e) {
+            throw new RuntimeException(e.toString());
+        }
+    }
+}
