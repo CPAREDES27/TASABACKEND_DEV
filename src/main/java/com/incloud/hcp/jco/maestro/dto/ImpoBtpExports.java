2e3d5fc2a86f4173988fe01278b7f92192ec0db2
pierotoscano <ptoscano@xternal.biz>
2021-12-06 18:11:22 -0500

    carga dinamica de archivos

--- src/main/java/com/incloud/hcp/jco/maestro/dto/ImpoBtpExports.java
+++ src/main/java/com/incloud/hcp/jco/maestro/dto/ImpoBtpExports.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class ImpoBtpExports {
     private List<HashMap<String, Object>> t_mensaje;
-    private String e_fieldName;
     private String mensaje;
 
     public List<HashMap<String, Object>> getT_mensaje() {
@@ -14,14 +13,6 @@ public class ImpoBtpExports {
 
     public void setT_mensaje(List<HashMap<String, Object>> t_mensaje) {
         this.t_mensaje = t_mensaje;
-    }
-
-    public String getE_fieldName() {
-        return e_fieldName;
-    }
-
-    public void setE_fieldName(String e_fieldName) {
-        this.e_fieldName = e_fieldName;
     }
 
     public String getMensaje() {
